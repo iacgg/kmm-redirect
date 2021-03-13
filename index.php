9eb0ebadd93f6cf01f958088be75624d01224ced
Sebastian Berendt <sb@peytz.dk>
2021-03-13 17:55:31 +0100

    test

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <html>
   <body background="https://assets.digitalocean.com/blog/static/hacktoberfest-is-back/hero.png">
   </body>
+</html>
+
 <?php
 sleep(5);
 // 301 Moved Permanently
 header("Location: https://khm.dk", true, 301);
 exit();
 ?>
-</html>
